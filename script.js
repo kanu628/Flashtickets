0ae6b745ddf4e2a0c518989f8c99a85b1f2beb3a
kanu628 <abkanu73@gmail.com>
2025-09-13 13:42:53 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const menuList = document.getElementById("menuList");
+
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
